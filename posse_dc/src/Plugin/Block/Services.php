3c4a39d5a9b56a9d8413a49811d96d3eff807eb3
AdrienneD <adriennedao@gmail.com>
2018-05-03 13:44:46 -0400

    created src

---
+++ posse_dc/src/Plugin/Block/Services.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\posse_dc\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Services' block.
+ *
+ * @Block(
+ *  id = "services",
+ *  admin_label = @Translation("Services"),
+ * )
+ */
+ class Services extends BlockBase {
+   
+   public function build() {
+     $build = [];
+     $build['services']['#markup']= 'Improve Your Online Presence';
+
+     return $build;
+   }
+ }
